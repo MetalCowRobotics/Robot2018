55a0a4f602070435e9068d403ccacdbb5d52691d
Tim <wayfarer1313@gmail.com>
2018-01-20 00:51:56 -0600

    Driver Contoller Stuff

--- src/org/usfirst/frc/team4213/robot/systems/DriveTrain.java
+++ src/org/usfirst/frc/team4213/robot/systems/DriveTrain.java
@@ -1,30 +1,67 @@
 package org.usfirst.frc.team4213.robot.systems;
 
 import org.usfirst.frc.team4213.robot.RobotMap;
+import org.usfirst.frc.team4213.robot.controllers.DriverController;
 
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.Talon;
 
 public class DriveTrain {
-	private static final SpeedController LEFT_MOTOR = new Victor(RobotMap.Drivetrain.LEFT_MOTOR_CHANNEL);
-	private static final SpeedController RIGHT_MOTOR = new Victor(RobotMap.Drivetrain.RIGHT_MOTOR_CHANNEL);
+	private static final Talon LEFT_MOTOR = new Talon(RobotMap.Drivetrain.LEFT_MOTOR_CHANNEL);
+	private static final Talon RIGHT_MOTOR = new Talon(RobotMap.Drivetrain.RIGHT_MOTOR_CHANNEL);
+	private DriverController controller;
 	
-	public void setLeftMotorSpeed(double speed){
-        System.out.print("LEFT: " + speed);
+	private int inverted = 1;
+	
+	DriveTrain(DriverController controller){
+		this.controller = controller;
+	}
+	
+	public void invert() {
+		inverted *= -1;
+	}
+	
+	private void setLeftMotorSpeed(double speed){
         LEFT_MOTOR.set(speed);
 	}
 	
-	public void setRightMotorSpeed(double speed){
-	    System.out.print("Right: "+ speed);
+	private void setRightMotorSpeed(double speed){
 		RIGHT_MOTOR.set(speed);
 	}
 	
-	public double getRightMotorSpeed(){
+	private double getRightMotorSpeed(){
 		return RIGHT_MOTOR.get();
 	}
 	
-	public double getLeftMotorSpeed(){
+	private double getLeftMotorSpeed(){
 		return LEFT_MOTOR.get();
+	}
+	
+	public void drive (boolean squareUnits) {
+		
+		if (controller.getAButton()){			//Go into half-arcade
+			setLeftMotorSpeed(-controller.getLY() * getThrottle() * inverted);
+			setRightMotorSpeed(controller.getLY() * getThrottle() * inverted);
+		} else {									//Stay in regular drive
+			setLeftMotorSpeed(-controller.getRY() * getThrottle() * inverted);
+			setRightMotorSpeed(controller.getLY() * getThrottle() * inverted);
+		}
+		
+	}
+	
+	/**
+	 * Determine the top speed threshold Bumper buttons on the controller will
+	 * limit the speed to the CRAWL value Trigger buttons on the controller will
+	 * limit the speed to the SPRINT value Otherwise it will allow the robot a
+	 * speed up to Normal max.
+	 */
+	private double getThrottle() {
+		if ( controller.getLB() ) {
+			return RobotMap.Drivetrain.CRAWL_SPEED;
+		} else if ( controller.getRB() ) {
+			return RobotMap.Drivetrain.SPRINT_SPEED;
+		} else {
+			return RobotMap.Drivetrain.NORMAL_SPEED;
+		}
 	}
 
 }
