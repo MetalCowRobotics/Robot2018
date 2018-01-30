c2ee3bb49be03919c2303ea7d4291890f7541075
Tim <wayfarer1313@gmail.com>
2018-01-30 00:12:26 -0600

    Leveraged WPI Differential Drive, tank and arcade.

--- src/org/usfirst/frc/team4213/robot/systems/DriveTrain.java
+++ src/org/usfirst/frc/team4213/robot/systems/DriveTrain.java
@@ -7,15 +7,19 @@ import org.usfirst.frc.team4213.robot.controllers.MasterControls;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class DriveTrain {
 	private static final DriveTrain instance = new DriveTrain();
 	private static final Logger logger = Logger.getLogger(DriveTrain.class.getName());
 
 	private MasterControls controller = MasterControls.getInstance();
+	
+	
 
 	private static final Talon LEFT_MOTOR = new Talon(RobotMap.Drivetrain.LEFT_MOTOR_CHANNEL);
 	private static final Talon RIGHT_MOTOR = new Talon(RobotMap.Drivetrain.RIGHT_MOTOR_CHANNEL);
+	private static final DifferentialDrive drive = new DifferentialDrive(LEFT_MOTOR, RIGHT_MOTOR);
 	private static final ADXRS450_Gyro gyroSPI = new ADXRS450_Gyro();
 	// gyroSPI = new ADXRS453Gyro();
 	// MY_GYRO = new AnalogGyro(RobotMap.Drivetrain.MY_GYRO_CHANNEL);
@@ -23,7 +27,7 @@ public class DriveTrain {
 	private int inverted = 1;
 
 	private DriveTrain() {
-
+		// Singleton
 	}
 
 	public static DriveTrain getInstance() {
@@ -39,29 +43,26 @@ public class DriveTrain {
 			invert();
 		}
 
-		double leftSpeed = squareSpeed(controller.getDriveLeftThrottle());
-		double rightSpeed = squareSpeed(controller.getDriveRightThrottle());
+		double leftSpeed = controller.getDriveLeftThrottle();
+		double rightSpeed = controller.getDriveRightThrottle();
 
-		if (controller.isHalfArcadeToggle()) { // Go into half-arcade
-			setLeftMotorSpeed(leftSpeed);
-			setRightMotorSpeed(leftSpeed);
-		} else { // Stay in regular drive
-			setLeftMotorSpeed(leftSpeed);
-			setRightMotorSpeed(rightSpeed);
-
+		if (controller.isHalfArcadeToggle()) { // Go into arcade mode
+			drive.arcadeDrive(leftSpeed, rightSpeed, true);
+		} else { // Stay in regular Tank drive mode
+			drive.tankDrive(leftSpeed, rightSpeed, true);
 		}
 
 		System.out.println("angle:" + gyroSPI.getAngle());
 
 	}
 
-	private double squareSpeed(double controllerSpeed) {
-		if (0 < controllerSpeed) {
-			return Math.pow(controller.getDriveLeftThrottle(), 2);
-		} else {
-			return -1 * Math.pow(controller.getDriveLeftThrottle(), 2);
-		}
-	}
+//	private double squareSpeed(double controllerSpeed) {
+//		if (0 < controllerSpeed) {
+//			return Math.pow(controller.getDriveLeftThrottle(), 2);
+//		} else {
+//			return -1 * Math.pow(controller.getDriveLeftThrottle(), 2);
+//		}
+//	}
 
 	public void invert() {
 		inverted *= -1;
