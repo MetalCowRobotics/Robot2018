8922604458d2cc35c04734be26827354f79041ce
Tim <wayfarer1313@gmail.com>
2018-01-29 19:36:53 -0600

    Elevator Singleton

--- src/org/usfirst/frc/team4213/robot/systems/Intake.java
+++ src/org/usfirst/frc/team4213/robot/systems/Intake.java
@@ -7,63 +7,74 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Talon;
 
 public class Intake {
-	MasterControls controller;
-	Elevator elevator;
-	
+	private static final Intake instance = new Intake();
+
+	private static final MasterControls controller = new MasterControls();
+	private static final Elevator elevator = new Elevator();
 	private static final Talon LEFT_INTAKE_MOTOR = new Talon(RobotMap.Intake.LEFT_MOTOR_CHANNEL);
 	private static final Talon RIGHT_INTAKE_MOTOR = LEFT_INTAKE_MOTOR;
-	//private static final Talon RIGHT_INTAKE_MOTOR = new Talon(RobotMap.Intake.RIGHT_MOTOR_CHANNEL);
-	
+	// private static final Talon RIGHT_INTAKE_MOTOR = new Talon(RobotMap.Intake.RIGHT_MOTOR_CHANNEL);
+
 	DigitalInput cubeSensorSwitch = new DigitalInput(RobotMap.Intake.LIMIT_SWITCH_CHANNEL);
-	
-	private enum IntakeState { OFF, IN, OUT };
+
+	private enum IntakeState {
+		OFF, IN, OUT
+	};
+
 	private IntakeState currentIntakeState = IntakeState.OFF; // start state is off
-	
-	public Intake(MasterControls controller, Elevator elevator) {
-		this.controller = controller;
-		this.elevator = elevator;
+
+	private Intake() {
+		// Singleton Pattern
 	}
-	
+
+	public Intake getInstance() {
+		return instance;
+	}
+
 	public void execute() {
-		 if (controller.isCubeIntake()) {
-			 powerCubeIntake();
-		 } else if (controller.isCubeEject()) {
-			 powerCubeEject();
-		 } else {
-			 powerCubeIdle();
-		 }
+		if (controller.isCubeIntake()) {
+			powerCubeIntake();
+		} else if (controller.isCubeEject()) {
+			powerCubeEject();
+		} else {
+			powerCubeIdle();
+		}
 	}
-	
+
 	private void powerCubeIntake() {
-		if (IntakeState.IN == currentIntakeState && !isCubeSensorSwitchActive()) { return; }
-		if(isCubeSensorSwitchActive()) {
+		if (IntakeState.IN == currentIntakeState && !isCubeSensorSwitchActive()) {
+			return;
+		}
+		if (isCubeSensorSwitchActive()) {
 			LEFT_INTAKE_MOTOR.setSpeed(RobotMap.Intake.INTAKE_SPEED);
 			RIGHT_INTAKE_MOTOR.setSpeed(RobotMap.Intake.INTAKE_SPEED);
 			currentIntakeState = IntakeState.IN;
 		} else {
 			powerCubeIdle();
 		}
-		
+
 	}
 
 	public void powerCubeEject() {
-		if (IntakeState.OUT == currentIntakeState) { return; }
+		if (IntakeState.OUT == currentIntakeState) {
+			return;
+		}
 		LEFT_INTAKE_MOTOR.setSpeed(RobotMap.Intake.EJECT_SPEED);
 		RIGHT_INTAKE_MOTOR.setSpeed(RobotMap.Intake.EJECT_SPEED);
 		currentIntakeState = IntakeState.OUT;
 	}
 
 	public void powerCubeIdle() {
-		if (IntakeState.OFF == currentIntakeState) { return; }
+		if (IntakeState.OFF == currentIntakeState) {
+			return;
+		}
 		LEFT_INTAKE_MOTOR.stopMotor();
 		RIGHT_INTAKE_MOTOR.stopMotor();
 		currentIntakeState = IntakeState.OFF;
 	}
-	
+
 	private boolean isCubeSensorSwitchActive() {
 		return cubeSensorSwitch.get();
 	}
-
-
 
 }
