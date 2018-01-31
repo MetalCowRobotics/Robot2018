8c6db67c80363d5c04f590928cc3859b0ff840f4
Tim <wayfarer1313@gmail.com>
2018-01-31 00:49:14 -0600

    uno mas.
    done for tonight.

--- src/org/usfirst/frc/team4213/autonomous/AutoMission1.java
+++ src/org/usfirst/frc/team4213/autonomous/AutoMission1.java
@@ -1,20 +1,28 @@
 package org.usfirst.frc.team4213.autonomous;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 public class AutoMission1 extends Mission {
+
+	// The Go Straight For X Feet Mission
 
 	public void execute() {
 		stepOne();
 	}
 
 	private void stepOne() {
-		double angle = driveTrain.getAngle();
-		// double Kp = 0.15;
+		// Drive Straight for 15 fee
 
-		double Kp = SmartDashboard.getNumber("Kp", .15);
-		System.out.println("Drive angle: " + (angle));
-		driveTrain.autoDrive(0, -angle * Kp);
+		// if(distanceTraveled < 15 feet) {
+		// double angle = driveTrain.getAngle();
+		// // double Kp = 0.15;
+		//
+		// figure out rou PID loop here for going stright.
+		//
+		// double Kp = SmartDashboard.getNumber("Kp", .15);
+		// System.out.println("Drive angle: " + (angle));
+		// driveTrain.autoDrive(0, -angle * Kp);
+		// } else {
+		// Step1 = COMPLETE;
+		// }
 	}
 
 }
