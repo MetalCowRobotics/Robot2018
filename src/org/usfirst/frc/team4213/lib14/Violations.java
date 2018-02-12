f9cabd28534e10d25456746f1c9ef80e2ea2a8de
deaner8765 <36179367+deaner8765@users.noreply.github.com>
2018-02-11 21:49:07 -0600

    remove complexity from violations

--- src/org/usfirst/frc/team4213/lib14/Violations.java
+++ src/org/usfirst/frc/team4213/lib14/Violations.java
@@ -8,19 +8,17 @@ public class Violations {
         double baseSpeed = 0;
         int a = 2, b = 3, a1 = 0, b1 = 0, a2 = 0, b2 = 0, c = 0, d = 1, c1 = 1, d1 = 12;
         if (a != b) { // 2
-            doIFs(a1, b1, a2, b2, c1, d1);
+            doIFs(a1, b1, a2, b2, c1);
         } else if (c != d) { // 7
-            while (c != d) { // 8
-                doNothing();
-            }
+            a += 1;
         }
-        if (34 == seconds && 67 > seconds || (baseSpeed == rightEncoder && baseSpeed == 56.7)) {
-            seconds = seconds + 3.5;
+        if (34 == seconds && 67 > a || (baseSpeed == rightEncoder && baseSpeed == 56.7)) {
             doNothing();
         }
     }
 
-    private void doIFs(int a1, int b1, int a2, int b2, int c1, int d1) {
+    private void doIFs(int a1, int b1, int a2, int b2, int c1) {
+        int d1 = 8;
         if (a1 != b1 // 3
                 && c1 != d1) { // 4
             doNothing();
