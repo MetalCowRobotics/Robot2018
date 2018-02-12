a396bbd036f5976f3a0f5b634acf68f74e199316
deaner8765 <36179367+deaner8765@users.noreply.github.com>
2018-02-11 21:44:41 -0600

    minor fixes

--- src/org/usfirst/frc/team4213/lib14/Violations.java
+++ src/org/usfirst/frc/team4213/lib14/Violations.java
@@ -4,32 +4,32 @@ public class Violations {
     private final int rightEncoder = 0;
 
     private void doCheckStyle() { // 1
-        //        double seconds = 0;
-        //        double baseSpeed = 0;
-        //        int a = 2, b = 3, a1 = 0, b1 = 0, a2 = 0, b2 = 0, c = 0, d = 1, c1 = 1, d1 = 12;
-        //        if (a != b) { // 2
-        //            doIFs(a1, b1, a2, b2, c1, d1);
-        //        } else if (c != d) { // 7
-        //            while (c != d) { // 8
-        //                doNothing();
-        //            }
-        //        }
-        //        if (34 == seconds && 67 > seconds || (baseSpeed == rightEncoder && baseSpeed == 56.7)) {
-        //            seconds = seconds + 3.5;
-        //            doNothing();
-        //        }
+        double seconds = 0;
+        double baseSpeed = 0;
+        int a = 2, b = 3, a1 = 0, b1 = 0, a2 = 0, b2 = 0, c = 0, d = 1, c1 = 1, d1 = 12;
+        if (a != b) { // 2
+            doIFs(a1, b1, a2, b2, c1, d1);
+        } else if (c != d) { // 7
+            while (c != d) { // 8
+                doNothing();
+            }
+        }
+        if (34 == seconds && 67 > seconds || (baseSpeed == rightEncoder && baseSpeed == 56.7)) {
+            seconds = seconds + 3.5;
+            doNothing();
+        }
     }
 
     private void doIFs(int a1, int b1, int a2, int b2, int c1, int d1) {
-        //        if (a1 != b1 // 3
-        //                && c1 != d1) { // 4
-        //            doNothing();
-        //        } else if (a2 != b2 // 5
-        //                || c1 < d1) { // 6
-        //            doNothing();
-        //        } else {
-        //            doNothing();
-        //        }
+        if (a1 != b1 // 3
+                && c1 != d1) { // 4
+            doNothing();
+        } else if (a2 != b2 // 5
+                || c1 < d1) { // 6
+            doNothing();
+        } else {
+            doNothing();
+        }
     }
 
     private void doNothing() {
