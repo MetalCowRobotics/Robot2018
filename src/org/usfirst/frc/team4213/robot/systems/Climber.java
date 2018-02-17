ab0e06da0bbbed573b12fe81c5e0f6f91ecdc9e6
metalcow <35390496+metalcow@users.noreply.github.com>
2018-02-16 20:25:47 -0600

    srx to talon

--- src/org/usfirst/frc/team4213/robot/systems/Climber.java
+++ src/org/usfirst/frc/team4213/robot/systems/Climber.java
@@ -2,12 +2,12 @@ package org.usfirst.frc.team4213.robot.systems;
 
 import java.util.logging.Logger;
 
-import org.usfirst.frc.team4213.lib14.MCR_SRX;
 import org.usfirst.frc.team4213.robot.RobotMap;
 import org.usfirst.frc.team4213.robot.controllers.MasterControls;
 
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
 
 public class Climber {
 	private static final Climber instance = new Climber();
@@ -15,8 +15,8 @@ public class Climber {
 
 	private static final MasterControls controller = MasterControls.getInstance();
 
-	private static final SpeedController CLIMBER_MOTOR1 = new MCR_SRX(RobotMap.Climber.CLIMBER_MOTOR_CHANNEL1);
-	private static final SpeedController CLIMBER_MOTOR2 = new MCR_SRX(RobotMap.Climber.CLIMBER_MOTOR_CHANNEL2);
+	private static final SpeedController CLIMBER_MOTOR1 = new Talon(RobotMap.Climber.CLIMBER_MOTOR_CHANNEL1);
+	private static final SpeedController CLIMBER_MOTOR2 = new Talon(RobotMap.Climber.CLIMBER_MOTOR_CHANNEL2);
 	private static SpeedControllerGroup ClimberSpeedControllerGroup = new SpeedControllerGroup (CLIMBER_MOTOR1, CLIMBER_MOTOR2);
 
 
